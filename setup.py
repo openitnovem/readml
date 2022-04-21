bcfb8bd6c1bcf8de09f943d4eecb6196af25889c
jontri <jonathan.tricard@sncf.fr>
2022-04-21 07:52:54 +0000

    FIX conditionnal python version for pickle5

--- setup.py
+++ setup.py
@@ -9,15 +9,17 @@ from version import __version__
 DESCRIPTION = "Interpretability DS Package"
 GIT_URL = "https://github.com/openitnovem/readml.git"
 AUTHOR_EMAIL = "readml-dev@gmail.com"
-REQUIREMENTS_PATH = os.path.join("requirements", "requirements.txt")
+REQUIREMENTS_PATH = os.path.join("requirements", "requirements_m1.txt")
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = "\n" + f.read()
 
+conditional_requires = ["pickle5==0.0.11; python_version < '3.8'"] # Pattern : package_name==0.0.00; python_version >= X.X
 with io.open(REQUIREMENTS_PATH, encoding="utf-8") as f:
     install_requires = [str(requirement) for requirement in parse_requirements(f)]
+    install_requires = conditional_requires + install_requires
 
 setup(
     name="readml",
